11b9f5aef831ca8fe8bc2654f3eda5e5b95c7ab4
MizG <mizgoines@users.noreply.github.com>
2015-07-17 16:15:12 -0400

    Updates to Lab
    Adding a View, Section 3

--- MvcMovie/Controllers/HelloWorldController.cs
+++ MvcMovie/Controllers/HelloWorldController.cs
@@ -8,18 +8,21 @@ namespace MvcMovie.Controllers
         // 
         // GET: /HelloWorld/ 
 
-        public string Index()
+        public ActionResult Index()
         {
-            return "This is my <b>default</b> action...";
+            return View();
         }
 
         // 
-        // GET: /HelloWorld/Welcome/ 
+        // 
 
-        public string Welcome(string name, int ID = 1)
-{
-    return HttpUtility.HtmlEncode("Hello " + name + ", ID: " + ID);
-}
+        public ActionResult Welcome(string name, int numTimes = 1)
+        {
+            ViewBag.Message = "Hello " + name;
+            ViewBag.NumTimes = numTimes;
+
+            return View();
+        }
     }
 }
 
